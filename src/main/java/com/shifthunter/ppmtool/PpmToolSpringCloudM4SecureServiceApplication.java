d4c376854e8e25d174541b27a695dfd5cf4ea95f
OsvaldoMartini <osvaldo.martini@gmail.com>
2019-10-28 21:58:06 +0100

    Enable Resource Server for OAuth2

--- src/main/java/com/shifthunter/ppmtool/PpmToolSpringCloudM4SecureServiceApplication.java
+++ src/main/java/com/shifthunter/ppmtool/PpmToolSpringCloudM4SecureServiceApplication.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
 @SpringBootApplication
 @RestController
+@EnableResourceServer
 public class PpmToolSpringCloudM4SecureServiceApplication {
 
 	public static void main(String[] args) {
